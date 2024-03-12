de79771752feeff785dd3ba409c500941331da1b
MOHAMMEDD ABDUL RAFAY <109493557+R02062004@users.noreply.github.com>
2024-03-12 16:50:47 +0530

    app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "your_secret_key"  # Set a secret key for session management
 
-notes = []
 @app.route('/', methods=["GET","POST"])
 def index():
     if request.method=="POST":
         note = request.form.get("note")
-        notes.append(note)
-    return render_template("home.html", notes=notes)
+        session.setdefault("notes",[]).append(note)  # Store notes in the session
+        
+    return render_template("home.html", notes= session.get("notes", []))
 
 
 if __name__ == '__main__':
